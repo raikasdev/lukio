d62c0066fa15566d2d50d4bd5d7ddfe3d3a114d7
raikasdev <roni.aikas@gmail.com>
2024-08-27 16:23:36 +0300

    Adjust tiedote AI prompt, switch to 4o

--- tiedotteet/index.ts
+++ tiedotteet/index.ts
@@ -16,7 +16,23 @@ async function parseEvents(weeklyBulletin: string): Promise<Calendar> {
   Upcoming events should be in an object of the same format but named "upcoming".
   Generic event information like a psychologist being available on x days shouldnt be marked as an event, but added to an "extra"-key to an array where one string is one extra thing.
   You need to work in Finnish.
-  If an even lasts for more than a day (for example exams on a specific week), it shouldn't be noted. Do not include "ylioppilaskirjoitukset" in the upcoming section.`
+  If an event lasts for more than a day (for example exams on a specific week), it shouldn't be noted. Do not include "ylioppilaskirjoitukset" in the upcoming section.
+  
+  If something is marked as happening on "odd weeks" or "even weeks", if the current week is odd/even like the event something like "(tällä viikolla)" should be added the declaration of odd/even weeks, otherwise "(ensi viikolla)". Example: "X on tavattavissa parittomilla viikoilla (tällä viikolla)" or "X on tavattavissa parillisilla viikoilla (ensi viikolla)". Keep a neutral tone.
+  
+  Classroom codes should be always uppercase, if they aren't you should uppercase them. They shouldn't be expanded. An suffix like "-lk" should be expanded to "-luokassa" or something similar in the correct inflection.
+  There are few exceptions to this rule. These classroom codes should be expanded, but only them:
+  - KU = "kuvataiteen luokka"
+  - PA_MU = "Paunun musiikkiluokka"
+
+  Here are abbreviations you should always expand:
+  - OHR = "opiskeluhuoltoryhmä"
+  - OPKH = "oppilaskunnan hallitus"
+  
+  All expanded abbreviations should be in Finnish and in the correct inflection.
+  
+  If an upcoming event includes the week day, even if abbreviated, it should be left out.
+  If an event has separate times for different classes, it should be noted as a single event but separated by newlines.`
   
   
   const response = await client.chat.completions.create({
@@ -24,7 +40,7 @@ async function parseEvents(weeklyBulletin: string): Promise<Calendar> {
       { role: 'system', content: SYSTEM_MESSAGE },
       { role: 'user', content: weeklyBulletin },
     ],
-    model: 'gpt-4o-mini',
+    model: 'gpt-4o',
     response_format: { type: 'json_object' },
   });
   
@@ -63,7 +79,7 @@ async function getWeeklyBulletin(): Promise<string> {
 
     const text = root.querySelector('#news-content')?.textContent;
     if (!text) {
-      console.error('Well shit no content');
+      console.error('Well shiiiit no content');
       process.exit(1);
     }
 
